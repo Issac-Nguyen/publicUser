4437ba24028f7477f484849dd7f0aeef64a2ad11
Chong Lun Chan <chancl@surbana.com>
2015-06-25 10:01:39 +0000

    Changes made by Chong Lun Chan (chancl@surbana.com).

--- requirejs-publicUser/scripts/app/views/defects.js
+++ requirejs-publicUser/scripts/app/views/defects.js
@@ -9,10 +9,32 @@ define(['jQuery', 'kendo', './template/baseTemplate', './defect'], function($, k
 		name: "baz",
 		description: 'des baz'
 	}];
+    
+     var groupedData1 = [
+        {name: "Sashimi salad", letter: "S" },
+        {name: "Chirashi sushi", letter: "C" },
+        {name: "Seaweed salad", letter: "S" },
+        {name: "Edamame", letter: "E" },
+        {name: "Miso soup", letter: "M" },
+        {name: "Maguro", letter: "M" },
+        {name: "Shake", letter: "S" },
+        {name: "Shiromi", letter: "S" },
+        {name: "Tekka maki", letter: "T" },
+        {name: "Hosomaki Mix", letter: "H" },
+        {name: "California rolls", letter: "C" },
+        {name: "Seattle rolls", letter: "S" },
+        {name: "Spicy Tuna rolls", letter: "S" },
+        {name: "Ebi rolls", letter: "E" },
+        {name: "Chicken Teriyaki", letter: "C" },
+        {name: "Salmon Teriyaki", letter: "S" },
+        {name: "Gohan", letter: "G" },
+        {name: "Tori Katsu", letter: "T" },
+        {name: "Yaki Udon", letter: "Y" }
+    ];
 	return {
 		init: function(initEvt) {
 			$("#listDefects").kendoMobileListView({
-				dataSource: groupedData,
+				dataSource: kendo.data.DataSource.create({data: groupedData1, group: "letter"}),
 				template: baseTemplate.templateDefect,
 				filterable: {
 					field: "name"
--- requirejs-publicUser/scripts/app/views/newDefect.js
+++ requirejs-publicUser/scripts/app/views/newDefect.js
@@ -1,22 +1,31 @@
 define(['kendo', '../phonegap/phonegap'], function(kendo, phonegap) {
-	return {
-		init: function(initEvt) {
-			// ... init event code ...
-		},
+    return {
+        init: function(initEvt) {
+            // ... init event code ...
+            $("#listImage").kendoMobileListView({
+                                                    dataSource: kendo.data.DataSource.create({data: [{dataURL: '1'}]}),
+                                                    template: "${dataURL}",
+                                                });
+        },
 
-		beforeShow: function(beforeShowEvt) {
-			// ... before show event code ...
-		},
+        beforeShow: function(beforeShowEvt) {
+            // ... before show event code ...
+        },
 
-		show: function(showEvt) {
-			// ... show event code ...
-		},
+        show: function(showEvt) {
+            // ... show event code ...
+        },
 
-		viewModel: kendo.observable({
-			message: 'new Defect',
-			addImage: function() {
-				phonegap.capturePicture();
-			}
-		}),
-	}
+        viewModel: kendo.observable({
+                                        message: 'new Defect',
+                                        source: [],
+                                        addImage: function() {
+                                            var self = this;
+                                            //phonegap.capturePicture(function(dataURL){
+                                            //    self.source.push({dataURL: dataURL});
+                                            //});
+                                            $("#listImage").data("kendoMobileListView").dataSource.add({dataURL: '2'});
+                                        }
+                                    }),
+    }
 });
